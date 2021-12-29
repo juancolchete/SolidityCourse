0cc9c464d25aa545bb6a6d5900972c17e08d3fc8
MarcioCV <marciocaldasvieira@outlook.com>
2021-12-29 19:08:00 -0300

    updating code

--- contracts/theMemeNFT.sol
+++ contracts/theMemeNFT.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract TheMemeNft is ERC721, ERC721URIStorage, Ownable {
+    using SafeMath for uint256;
     using Counters for Counters.Counter;
     mapping(address=>mapping(uint256=>uint256)) public stake;
-
+    uint256 [] private rarities;
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("The Meme Nft", "TMN") {}
@@ -19,6 +21,7 @@ contract TheMemeNft is ERC721, ERC721URIStorage, Ownable {
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+        rarities = [1, 3, 7, 10];
     }
 
     // The following functions are overrides required by Solidity.
@@ -33,8 +36,19 @@ contract TheMemeNft is ERC721, ERC721URIStorage, Ownable {
     }
 
     function startStake(uint256 idToken) public{
+        require(ERC721.ownerOf(idToken) == msg.sender,"You are not the NFT owner !.");
         require(!isStake(idToken,msg.sender),"Your NFT is already in stake, choose another NFT to stake");
         stake[msg.sender][idToken] = block.number;
+    }
+    
+    function stopStake(uint256 idToken) public{
+        require(ERC721.ownerOf(idToken) == msg.sender,"You are not the NFT owner !.");
+        require(isStake(idToken,msg.sender),"Your NFT is not in stake, choose another NFT to remove from stake");
+        stake[msg.sender][idToken] = 0;
+    }
+
+    function _beforeTokenTransfer(address from,address to,uint256 idToken) internal view override{
+        require(!isStake(idToken,msg.sender),"Your NFT is in stake, choose another NFT to stake");
     }
 
     function isStake(uint256 idToken, address end) public view returns(bool){
@@ -44,5 +58,13 @@ contract TheMemeNft is ERC721, ERC721URIStorage, Ownable {
         else{
             return true;
         }
+    }
+
+    function calcRewards(uint256 idToken, address owner) public view returns(uint256){
+        require(_exists(idToken),"Token that you have mention does not exist.");
+        require(isStake(idToken,owner),"Your NFT is not stake, choose another NFT to calculate");
+        uint256 calc = block.number.sub(stake[msg.sender][idToken]);
+        calc = calc.div(28800);
+        return calc;
     }
 }
