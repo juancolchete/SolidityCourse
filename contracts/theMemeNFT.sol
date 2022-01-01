b4c95256114e932026d29fe1730e4948a8bd122f
MarcioCV <marciocaldasvieira@outlook.com>
2022-01-01 19:08:12 -0300

    updating code

--- contracts/theMemeNFT.sol
+++ contracts/theMemeNFT.sol
@@ -6,10 +6,15 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract TheMemeNft is ERC721, ERC721URIStorage, Ownable {
+    using SafeERC20 for IERC20;
     using SafeMath for uint256;
     using Counters for Counters.Counter;
+    IERC20 private _token;
+    address payable rewardWallet;
     mapping(address=>mapping(uint256=>uint256)) public stake;
     string[] private rarities;
     // Rarity => Reward
@@ -17,13 +22,16 @@ contract TheMemeNft is ERC721, ERC721URIStorage, Ownable {
     // TokenId => Rarity
     mapping(uint256 => uint16) private _tokenRarity;
     Counters.Counter private _tokenIdCounter;
+    uint256 private stakeperiod;
 
-    constructor() ERC721("The Meme Nft", "TMN") {
-        _rewardRarity[0] = 3472222222222;
-        _rewardRarity[1] = 34722222222222;
-        _rewardRarity[2] = 347222222222222;
-        _rewardRarity[3] = 3472222222222222;
+    constructor(address payable token) ERC721("The Meme Nft", "TMN") {
+        _rewardRarity[0] = 1000000000000;
+        _rewardRarity[1] = 10000000000000;
+        _rewardRarity[2] = 100000000000000;
+        _rewardRarity[3] = 1000000000000000000;
+        stakeperiod = 604800;
         rarities = ["garbage","simple", "rare","legend"];
+        _token = token;
     }
 
     function safeMint(address to, string memory uri) public onlyOwner {
@@ -48,18 +56,21 @@ contract TheMemeNft is ERC721, ERC721URIStorage, Ownable {
     function startStake(uint256 idToken) public{
         require(ERC721.ownerOf(idToken) == msg.sender,"You are not the NFT owner !.");
         require(!isStake(idToken,msg.sender),"Your NFT is already in stake, choose another NFT to stake");
-        stake[msg.sender][idToken] = block.number;
+        stake[msg.sender][idToken] = block.timestamp;
     }
     
     function stopStake(uint256 idToken) public{
         require(ERC721.ownerOf(idToken) == msg.sender,"You are not the NFT owner !.");
         require(isStake(idToken,msg.sender),"Your NFT is not in stake, choose another NFT to remove from stake");
+        require(block.timestamp.sub(stake[msg.sender][idToken]) >= stakeperiod,"You do not complete a week");
+        calcRewards(idToken,msg.sender);
+        sendRewards(calcRewards(idToken,msg.sender));
         stake[msg.sender][idToken] = 0;
     }
 
     function _beforeTokenTransfer(address from,address to,uint256 idToken) internal view override{
         require(!isStake(idToken,msg.sender),"Your NFT is in stake, choose another NFT to stake");
-    }
+    }              
 
     function isStake(uint256 idToken, address end) public view returns(bool){
         if(stake[end][idToken] <= 0){
@@ -71,10 +82,15 @@ contract TheMemeNft is ERC721, ERC721URIStorage, Ownable {
     }
 
     function calcRewards(uint256 idToken, address owner) public view returns(uint256){
-        require(_exists(idToken),"Token that you have mention does not exist.");
-        require(isStake(idToken,owner),"Your NFT is not stake, choose another NFT to calculate");
-        uint256 calc = block.number.sub(stake[msg.sender][idToken]);
-        calc = calc.mul(_rewardRarity[_tokenRarity[idToken]]);
-        return calc;
+        uint256 semana = block.timestamp.sub(stake[owner][idToken]).div(stakeperiod);
+        return _rewardRarity[_tokenRarity[idToken]].mul(semana);
+    }
+    
+    function setPeriod(uint256 period) public onlyOwner{
+        stakeperiod = period;
+    }
+
+    function sendRewards(uint256 quant) private{
+        _token.SafeTransferFrom(rewardWallet,msg.sender,quant);
     }
 }
