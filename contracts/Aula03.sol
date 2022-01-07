1976ae6f3630a8be22392ce91384fe0749d23642
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2022-01-07 12:12:18 -0300

    Fix Aula03 return and localVariable function

--- contracts/Aula03.sol
+++ contracts/Aula03.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 contract Aula03{
     uint256 stateVariable;
 
-    function pureSample(uint256 localVariable) pure {
-        return stateVariable;
+    function pureSample(uint256 localVariable) pure public returns(uint256){
+        return localVariable;
     }
-    function viewSample(uint256 localVariable) view{
+    function viewSample(uint256 localVariable) view public returns(uint256){
         return stateVariable;
     }
 }
