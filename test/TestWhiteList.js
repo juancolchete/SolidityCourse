39094e76405ff593b56bbd31caee6ca7de5ffa2f
juanudk <juanudkp@gmail.com>
2022-02-19 01:30:50 -0300

    Change addresses to work better

--- test/TestWhiteList.js
+++ test/TestWhiteList.js
@@ -1,7 +1,8 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 let signerAddresses;
-
+let firstAddress = "0x1D8989d956A015AE1333d9ee2a7a42b403426dFd";
+let secondAddress = "0xBd3D3C0bCc3FBdDedd019277012ab2da6BB294F5";
 
 describe("WhiteList", () =>{
     
@@ -12,17 +13,17 @@ describe("WhiteList", () =>{
         whitelist.addToWhiteList(signerAddresses[0].address);
     });
     it("Should print that you are in the whitelist", async ()=>{
-        await whitelist.addToWhiteList("0x1D8989d956A015AE1333d9ee2a7a42b403426dFd")
-        let printResponse = await whitelist.isInList("0x1D8989d956A015AE1333d9ee2a7a42b403426dFd");
+        await whitelist.addToWhiteList(firstAddress)
+        let printResponse = await whitelist.isInList(firstAddress);
         expect(printResponse).to.be.equal(true);
     });
     it("Should print that you are not in the whitelist", async ()=>{
-        let printResponse = await whitelist.isInList("0xBd3D3C0bCc3FBdDedd019277012ab2da6BB294F5");
+        let printResponse = await whitelist.isInList(secondAddress);
         expect(printResponse).to.be.equal(false);
     });
     it("Should remove the address from the whitelist and check if the address is really out", async ()=>{
-        await whitelist.remove("0x1D8989d956A015AE1333d9ee2a7a42b403426dFd")
-        let printResponse = await whitelist.isInList("0x1D8989d956A015AE1333d9ee2a7a42b403426dFd");
+        await whitelist.remove(firstAddress)
+        let printResponse = await whitelist.isInList(firstAddress);
         expect(printResponse).to.be.equal(false);
     });
     it("Should only allow address from whitelist to execute", async ()=>{
