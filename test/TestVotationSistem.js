18197e7b395b3d37746bf1a66edb3078893ecddd
MarcioCV <marciocaldasvieira@outlook.com>
2022-03-10 19:15:34 -0300

    Update TestVotationSistem.js
    Co-Authored-By: Juan Colchete <38388534+juanudk@users.noreply.github.com>

--- test/TestVotationSistem.js
+++ test/TestVotationSistem.js
@@ -35,7 +35,15 @@ describe("TestVotationSistem", () =>{
             {account: signers[4].address,trustToken: 100, untrustToken: 90},
         ]
         await addBallance(balanceData);
-    })      
+    });  
+    it("Should add 3 unlegible accounts",async ()=>{
+        let balanceData = [
+            {account: signers[5].address,trustToken: 900, untrustToken: 900},
+            {account: signers[6].address,trustToken: 100, untrustToken: 110},
+            {account: signers[7].address,trustToken: 0, untrustToken: 1000},
+        ]
+        await addBallance(balanceData);
+    });  
     it("Should insert proposal on the sistem ", async ()=>{
         await testVotationSistem.connect(signers[3]).addProposal("Change all school drinking fountain for coke");
         let printResponse = await testVotationSistem.checkProposal(signers[3].address);
@@ -46,14 +54,30 @@ describe("TestVotationSistem", () =>{
         let printResponse = await testVotationSistem.checkProposal(signers[3].address);
         expect(printResponse).to.be.equal("");
     });
-    it("Should insert two proposal on the sistem ", async ()=>{
+    it("Should insert three proposal on the sistem ", async ()=>{
+        await testVotationSistem.connect(signers[2]).addProposal("Make the revolution #aa61564cbbb012b5b198d58a1ad51314");
         await testVotationSistem.connect(signers[3]).addProposal("Change all school drinking fountain for coke");
         await testVotationSistem.connect(signers[4]).addProposal("Change school uniforms to normal clothes");
+        printResponse = await testVotationSistem.checkProposal(signers[2].address);
+        expect(printResponse).to.be.equal("Make the revolution #aa61564cbbb012b5b198d58a1ad51314");
         let printResponse = await testVotationSistem.checkProposal(signers[3].address);
         expect(printResponse).to.be.equal("Change all school drinking fountain for coke");
         printResponse = await testVotationSistem.checkProposal(signers[4].address);
         expect(printResponse).to.be.equal("Change school uniforms to normal clothes");
     });
+
+    it("Should not allow the vote to be registered",async ()=>{
+       await expect(testVotationSistem.connect(signers[5]).addVote(signers[3].address)).to.be.revertedWith("You can't vote, try be a good citizen and come back later !");
+       await expect(testVotationSistem.connect(signers[6]).addVote(signers[3].address)).to.be.revertedWith("You can't vote, try be a good citizen and come back later !");
+       await expect(testVotationSistem.connect(signers[7]).addVote(signers[3].address)).to.be.revertedWith("You can't vote, try be a good citizen and come back later !");
+    });
+
+    it("Should not allow the proposal to be registered",async ()=>{
+        await expect(testVotationSistem.connect(signers[5]).addProposal("Change all school drinking fountain for coke")).to.be.revertedWith("You can't vote, try be a good citizen and come back later !");
+        await expect(testVotationSistem.connect(signers[6]).addProposal("Change all school drinking fountain for coke")).to.be.revertedWith("You can't vote, try be a good citizen and come back later !");
+        await expect(testVotationSistem.connect(signers[7]).addProposal("Change all school drinking fountain for coke")).to.be.revertedWith("You can't vote, try be a good citizen and come back later !");
+     });
+
     it("Should register a vote ", async ()=>{
         await testVotationSistem.connect(signers[4]).addVote(signers[3].address)
         let voterBefore = await testVotationSistem.checkVotes(signers[3].address);
@@ -63,8 +87,7 @@ describe("TestVotationSistem", () =>{
     it("Should pick the winner, that is the 3 signer proposal", async () =>{
         let printResponse = await testVotationSistem.pickWinner();
         printResponse = await printResponse.wait()
-        console.log(printResponse)
-        expect(printResponse[0]).to.be.equal(signers[3].address);
-    })
+        expect(printResponse.events[0].args.winnerAddress).to.be.equal(signers[3].address);
+    });
 });
 
